63c47779dafec073af45512c0d8536a450f3cf17
Christopher Chan <christopher.chan2014@gmail.com>
2019-10-31 23:39:30 -0400

    cleanup logging

--- logging/fake_app.py
+++ logging/fake_app.py
@@ -3,12 +3,14 @@
 import logging
 import fake_lib
 
+
 def main():
 
 	logging.basicConfig(filename = 'fakeapp.log', level = logging.INFO)
 	logging.info('Started')
 	fake_lib.do_something()
 	logging.info('Finished')
-	
+
+
 if __name__ == '__main__':
 	main()
