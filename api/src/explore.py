fb9425eb3a49a453c34306d3bb28b3e99ab26b54
chriscross00 <christopher.chan2014@gmail.com>
2021-04-25 21:57:41 -0400

    1st successful api call from python

--- api/src/explore.py
+++ api/src/explore.py
@@ -8,5 +8,15 @@ load_dotenv()
 
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+FROM_PHONE = os.environ.get('FROM_PHONE')
+TO_PHONE = os.environ.get('TO_PHONE')
 
-client = Client()
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+message = client.messages.create(
+    body = 'This is from a python api call',
+    from_ = FROM_PHONE,
+    to = TO_PHONE
+)
+
+print(message.sid)
